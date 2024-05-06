0cae3ac1ee18fff4f742cf11c3a41e52fc811339
ali-consciousroad <ali_dindar@live.be>
2024-05-06 14:59:28 +0200

    Complete and test the projects with different values

--- mealMaker.js
+++ mealMaker.js
@@ -1,35 +1,48 @@
 const menu = {
     // private key pairs
-    _meal: 25,
+    _meal: "",
     _price: 0,
 
     set meal(mealToCheck){
         if(typeof(mealToCheck) === "string"){
-            return this._meal = mealToCheck;
+            this._meal = mealToCheck;
         }
     },
 
     set price(priceToCheck){
         if(typeof(priceToCheck) === "number"){
-            return this._price = priceToCheck;
+            this._price = priceToCheck;
+        }
+    },
+
+    // Don't check other falsy values like undefined, 0, NaN, "" or false!
+    /* get todaysSpecial(){
+        if(menu._meal != null && menu._price != null){
+            return `Today's Special is ${this._meal} for ${this._price}`
+        } 
+        else {
+            return "Meal or price was not set correctly!";
+        }
+    } */
+    get todaysSpecial(){
+        if(this._meal && this._price){
+            return `Today's Special is ${this._meal} for ${this._price}`
+        } 
+        else {
+            return "Meal or price was not set correctly!";
         }
     }
+
 }
 
+// Test
+// Will give an error
+// menu.meal = 5;
+// menu.price = "test";
+
 menu.meal = "fesenjoun";
-menu.price = 15;
+menu.price = 25;
 
+console.log(menu.price);
 console.log(menu);
-
-// console.log(student.firstName); // Monica
-
-// // change(set) object property using a setter
-// student.changeName = 'Sarah';
-
-// console.log(student.firstName); // Sarah
-
-// student.changeName = "Maria";
-
-// console.log(student.firstName);
-
-// console.log(menu);
+console.log(menu.todaysSpecial);
